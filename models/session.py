b0cad41286951d72b6b1a2a52473562afd385c0a
Tomas Robino <tomaserobino@gmail.com>
2025-11-30 20:16:18 +0100

    better domains

--- models/session.py
+++ models/session.py
@@ -6,11 +6,17 @@ class session(models.Model):
 
     name = fields.Char(required=True)
     start_date = fields.Date(default=fields.Date.today)
+    active = fields.Boolean(default=True)
     duration = fields.Integer()
     seats = fields.Integer()
     instructor = fields.Many2one(
         'res.partner',
-        domain="[('instructor', '=', True)]",
+        domain="""
+            ['|',
+                ('instructor', '=', True),
+                ('category_id', 'child_of', ref('open_academy.cat_teacher'))
+            ]
+            """,
     )
     course_id = fields.Many2one('open_academy.course')
     attendees = fields.Many2many('res.partner')
