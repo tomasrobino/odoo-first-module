8546e30aed0f8c995404350180b7e883ea0c3ca5
Tomas Robino <tomaserobino@gmail.com>
2026-02-19 16:18:46 +0100

    security users

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,8 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
